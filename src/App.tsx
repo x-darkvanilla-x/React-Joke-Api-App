46d3987f9b60a738e71f79359ec62b26c9c476d6
Dipesh <dipesh.adelkar@gmail.com>
2024-05-16 21:35:30 +0530

    bg height

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ export const App = () => {
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
-        minHeight: "100vh",
+        minHeight: "100svh",
         backgroundColor: "#A020F0",
       }}
     >
